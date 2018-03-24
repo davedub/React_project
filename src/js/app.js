0887e7b3ae972f0533849133203ca8bb051e0411
davedub <dwitus@gmail.com>
2018-03-24 13:59:08 -0700

    change component to a class and use render method to return state of class

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Headline = () => {
-    return <h1 className="title">Welcome to the world of React!!</h1>
-}
 
-const Greeting = (props) => {
-    const {name, age} = props;
-    return <p>This greeting is for {name} who is {age} years old.</p>
-}
 export class App extends React.Component {
-    render() {
-    return (
-     <div>
-     <Headline />   
-     <Greeting name="Bill" age={36} /> 
-     </div>
-     )
-   }
-}
 
-
-Greeting.propTypes = {
-    name: PropTypes.string.isRequired,
-    age: PropTypes.number
-};
+    constructor(props) {
+        super(props);
+        this.state = {   // a simple object containing the state of the app
+             buyItems: ['milk', 'bread', 'fruit']
+            }
+        }
+        render() {
+            const {buyItems} = this.state;
+            return (
+            <div>
+                <h1>Shopping List</h1>
+                {
+                buyItems.map(item => {
+                        return <p key={item}>{item}</p>
+                })
+            }
+            </div>
+            )
+        }
+    }
