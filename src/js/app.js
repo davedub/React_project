770e45caa400d50568d150165a44ab2476f2506a
davedub <dwitus@gmail.com>
2018-03-24 13:29:43 -0700

    a simple class example for app

--- src/js/app.js
+++ src/js/app.js
@@ -9,15 +9,17 @@ const Greeting = (props) => {
     const {name, age} = props;
     return <p>This greeting is for {name} who is {age} years old.</p>
 }
-
-export const App = () => {
+export class App extends React.Component {
+    render() {
     return (
      <div>
      <Headline />   
-     <Greeting name="Steve" age={36} /> 
+     <Greeting name="Bill" age={36} /> 
      </div>
      )
+   }
 }
+
 
 Greeting.propTypes = {
     name: PropTypes.string.isRequired,
